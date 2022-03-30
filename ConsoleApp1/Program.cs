2f9eaa3e06bed564109098cdf34a5cd3751709b2
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-03-30 12:33:52 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar coordenadas");
+            
+            rectangulo rectangulo_cal = new rectangulo();
+
+            Console.WriteLine("Ingresar x1");
+            rectangulo_cal.x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar y1");
+            rectangulo_cal.y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar x2");
+            rectangulo_cal.x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar y2");
+            rectangulo_cal.y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar x3");
+            rectangulo_cal.x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar y3");
+            rectangulo_cal.y3 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Perimetro: " + 2*(rectangulo_cal.calcularDistancia() + rectangulo_cal.calcularDistancia2()) );
+            Console.WriteLine("Área: " + (rectangulo_cal.calcularDistancia() * rectangulo_cal.calcularDistancia2()) );
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ ConsoleApp1/rectangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class rectangulo
+    {
+        public double x1 { get; set; }
+        public double x2 { get; set; }
+        public double y1 { get; set; }
+        public double y2 { get; set; }
+        public double x3 { get; set; }
+        public double y3 { get; set; }
+
+        public double base_rec { get; set; }
+        public double altu_rec { get; set; }
+
+        public double calcularDistancia() {
+            base_rec = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return base_rec;
+        }
+
+        public double calcularDistancia2() {
+            altu_rec = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+            return altu_rec;
+        }
+    }
+}
